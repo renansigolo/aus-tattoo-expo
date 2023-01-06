2fd0e59e4ab48c4941b24c45ccf80a05a071bd82
Renan Sigolo <renan.sigolo@gmail.com>
2023-01-06 13:58:09 +1100

    fix: whitelist server address

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [
+      '13.211.134.5', // lightsail server
       'aus-tattoo-expo.local',
       'placeholder.pics',
       'images.unsplash.com',
