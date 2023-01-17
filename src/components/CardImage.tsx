d843f6deca489483fedc726c233168368467ab38
Renan Sigolo <renan.sigolo@gmail.com>
2023-01-17 19:33:30 +1100

    fix: images

--- src/components/CardImage.tsx
+++ src/components/CardImage.tsx
@@ -1,4 +1,5 @@
 import { WPImage } from "@/lib/utils/types"
+import Image from "next/image"
 import Link from "next/link"
 
 type CardImageProps = {
@@ -12,7 +13,7 @@ export function CardImage({ image, title, description, url }: CardImageProps) {
     <>
       <div className="relative">
         <div className="group block aspect-square w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-primary-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-600">
-          <img
+          <Image
             src={image.sourceUrl}
             alt={image.altText || image.title}
             className="pointer-events-none min-w-full object-cover group-hover:opacity-75"
