951387461e5191266a9f4a96b42e3e99223d79db
Renan Sigolo <renan.sigolo@gmail.com>
2023-01-29 17:27:30 +1100

    fix: social icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,10 @@ import { Open_Sans } from "@next/font/google"
 import type { AppProps } from "next/app"
 import client from "src/apollo/client"
 
+import { config } from "@fortawesome/fontawesome-svg-core"
+import "@fortawesome/fontawesome-svg-core/styles.css"
+config.autoAddCss = false
+
 const openSans = Open_Sans({
   subsets: ["latin"],
   variable: "--font-open-sans",
