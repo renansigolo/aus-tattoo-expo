d7236bfa565c8efa45248bd0b33a46cd27eb385c
Renan Sigolo <renan.sigolo@gmail.com>
2023-02-09 16:54:54 +1100

    feat: add modal to gallery

--- src/components/flexible/Gallery.tsx
+++ src/components/flexible/Gallery.tsx
@@ -1,5 +1,7 @@
 import { Grid } from "@/components/layout/Grid"
+import { Modal } from "@/components/overlays/Modal"
 import { WPImage } from "@/lib/utils/types"
+import { useState } from "react"
 
 type GalleryProps = {
   gallery: {
@@ -9,19 +11,30 @@ type GalleryProps = {
 }
 
 export function Gallery({ gallery }: GalleryProps) {
+  const [open, setOpen] = useState(false)
+  const [imageRef, setImageRef] = useState("")
+
   return (
-    <Grid columns={gallery.columns || 1}>
-      {gallery.images.map((image: any, index: number) => (
-        <div key={index} className="max-w-full rounded-sm">
-          <img
-            src={image.sourceUrl}
-            alt={image.altText || image.title}
-            width={1024}
-            height={1024}
-            className="w-full object-contain"
-          />
-        </div>
-      ))}
-    </Grid>
+    <>
+      <Grid columns={gallery.columns || 1}>
+        {gallery.images.map((image: any, index: number) => (
+          <div key={index} className="max-w-full rounded-sm">
+            <img
+              src={image.sourceUrl}
+              alt={image.altText || image.title}
+              width={1024}
+              height={1024}
+              className="aspect-square w-full object-cover hover:cursor-pointer"
+              onClick={() => {
+                setImageRef(image.sourceUrl)
+                setOpen(true)
+              }}
+            />
+          </div>
+        ))}
+      </Grid>
+
+      <Modal sourceUrl={imageRef} open={open} setOpen={setOpen} />
+    </>
   )
 }
