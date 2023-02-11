b3515565345d1d0627374f91cbda97a9571b290d
Renan Sigolo <renan.sigolo@gmail.com>
2023-02-11 12:46:46 +1100

    fix: add meta verification

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Head, Html, Main, NextScript } from "next/document"
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="xXtVxGeQmxZrCFc-HHQfkv6_bDUuycNU9fjzzUtMgdY"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
